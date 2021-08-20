d15e21e79786bdcdb4716fad6661cd697e2507a0
Andrey Shulmin <ashulmin14@gmail.com>
2021-08-21 01:04:54 +0300

    added dotenv file

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
-import express from 'express';
+import express, { Application, Request, Response } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const app = express();
-const port = 3000;
-app.get('/', (req, res) => {
+const app: Application = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
   res.send('The sedulous hyena ate the antelope!');
 });
 
