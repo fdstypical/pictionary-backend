b9c937d39d34f1b6c755797ed7d94b61fb6337c9
Andrey Shulmin <ashulmin14@gmail.com>
2021-08-22 13:17:40 +0300

    add base App class

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,26 @@ import { config, Routes } from '../configs';
 import UsersRouter from './routers/UsersRouter';
 import RoomsRouter from './routers/RoomsRouter';
 
-const app: Application = express();
-app.use(express.json());
+export class App {
+  private static _instance: App;
+  private _app: Application;
 
-app.use(`${config.API_PREFIX}${Routes.users}`, UsersRouter);
-app.use(`${config.API_PREFIX}${Routes.rooms}`, RoomsRouter);
+  private constructor(private readonly _port: string | number = config.PORT) {
+    this._app = express();
+    this._app.use(express.json());
 
-app.listen(config.PORT, () => {
-  return console.log(`server is listening on ${config.PORT}`);
-});
+    this._app.use(`${config.API_PREFIX}${Routes.users}`, UsersRouter);
+    this._app.use(`${config.API_PREFIX}${Routes.rooms}`, RoomsRouter);
+  }
+
+  public static get Instance(): App {
+    return this._instance || (this._instance = new this());
+  }
+
+  public init(): void {
+    this._app.listen(this._port, () => console.log(`App listen on port: ${this._port}`));
+  }
+}
+
+const app = App.Instance;
+app.init();
