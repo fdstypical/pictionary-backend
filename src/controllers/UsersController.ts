8197c39df597cb7dbc1a5e1cd879c5a4be28d56d
Andrey Shulmin <ashulmin14@gmail.com>
2021-09-03 21:07:19 +0300

    update users crud

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -6,6 +6,27 @@ class UsersController {
     const users = await User.findAll({ include: [{ model: Room }] });
     res.json(users);
   }
+
+  async getOneUser(req: Request, res: Response) {
+    const { id } = req.params;
+    const user = await User.findByPk(id, { include: [{ model: Room }] });
+    res.json(user);
+  }
+
+  async patchUser(req: Request, res: Response) {
+    const { id } = req.params;
+    const { name } = req.body;
+
+    if (!name) {
+      res.status(400).json({ message: 'Name is required' });
+    }
+
+    const [, [updatedUser]] = await User.update(
+      { name },
+      { where: { id }, returning: true },
+    );
+    res.json(updatedUser);
+  }
 }
 
 export default new UsersController();
--- src/routers/UsersRouter.ts
+++ src/routers/UsersRouter.ts
@@ -7,5 +7,7 @@ const UsersRouter: Router = express.Router();
 
 UsersRouter.use(checkAuth);
 UsersRouter.get('/', UsersController.getUsers);
+UsersRouter.get('/:id', UsersController.getOneUser);
+UsersRouter.patch('/:id', UsersController.patchUser);
 
 export default UsersRouter;
