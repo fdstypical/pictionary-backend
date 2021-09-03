b7f7c4e4c1ab553130abc6fa61f0e21df00cc318
Andrey Shulmin <ashulmin14@gmail.com>
2021-09-03 22:13:35 +0300

    update rooms crud

--- src/controllers/RoomsController.ts
+++ src/controllers/RoomsController.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express';
+import httpContext from 'express-http-context';
+
 import { User, Room } from '../models';
 
 class RoomsController {
@@ -7,26 +9,40 @@ class RoomsController {
     res.json(rooms);
   }
 
+  async getRoom(req: Request, res: Response) {
+    const { id } = req.params;
+    const room = await Room.findByPk(id, { include: [{ model: User }] });
+    res.json(room);
+  }
+
   async createRoom(req: Request, res: Response) {
     const { name } = req.body;
     const room = await Room.create({ name });
     res.json(room);
   }
 
+  async deleteRoom(req: Request, res: Response) {
+    const { id } = req.params;
+    const code = await Room.destroy({ where: { id } });
+    res.json({ code });
+  }
+
   async addUser(req: Request, res: Response) {
     const roomId = req.params.id;
-    const userId = req.params.userId;
-
-    const room: any = await Room.findByPk(roomId, { include: [{ model: User }] });
-    if (!room) return res.status(400).json({ message: 'Room not found' });
+    const { id: userId } = httpContext.get('user');
 
     const user = await User.findByPk(userId);
     if (!user) return res.status(400).json({ message: 'User not found' });
+
+    const room: any = await Room.findByPk(roomId, { include: [{ model: User }] });
+    if (!room) return res.status(400).json({ message: 'Room not found' });
 
     const updatedRoom = await room.addUser(user);
     const reloaderRoom = await updatedRoom.reload({ include: [{ model: User }] });
     res.json(reloaderRoom);
   }
+
+  async leaveRoom(req: Request, res: Response) {}
 }
 
 export default new RoomsController();
--- src/routers/RoomsRouter.ts
+++ src/routers/RoomsRouter.ts
@@ -7,7 +7,10 @@ const RoomsRouter: Router = express.Router();
 
 RoomsRouter.use(checkAuth);
 RoomsRouter.get('/', RoomsController.getRooms);
+RoomsRouter.get('/:id', RoomsController.getRoom);
 RoomsRouter.post('/', RoomsController.createRoom);
-RoomsRouter.post('/:id/:userId', RoomsController.addUser);
+RoomsRouter.post('/enter/:id', RoomsController.addUser);
+RoomsRouter.post('/leave/:id', RoomsController.leaveRoom);
+RoomsRouter.delete('/:id', RoomsController.deleteRoom);
 
 export default RoomsRouter;
